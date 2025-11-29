77add99185b8df6e5ab38b645644068ccdac6479
Ronaldo <ronaldo-troupe@bitbucket.org>
2025-11-29 15:58:52 -0300

    feat: Added prisma client configuration and add  readme backend

---
+++ backend/README.md
@@ -0,0 +1,8 @@
+## Create .env file in root backend directory
+`cp .env.example .env`
+
+## Build containers of the backend:
+`npm run services:up`
+
+## Create new migration:
+`migrate:create` 
---
+++ backend/src/config/database.ts
@@ -0,0 +1,16 @@
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client'
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+
+const prismaClient = () => new PrismaClient({ adapter })
+
+declare const globalThis: {
+    prismaGlobal: ReturnType<typeof prismaClient>
+} & typeof global
+
+const prisma = globalThis.prismaGlobal ?? prismaClient
+
+if(process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+
+export default prisma
