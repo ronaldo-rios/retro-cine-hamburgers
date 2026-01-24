780d88dc01755762684b2e427ff06c148f6a9c3e
Ronaldo <ronaldo-troupe@bitbucket.org>
2026-01-24 14:59:28 -0300

    feat: Created food menu for Home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,23 @@
-import { Link } from "react-router"
+import { useState } from "react"
+import Button from "../components/Button"
 
 const Home = () => {
+
+    const categories = ['Burgers', 'Bebidas', 'Porções']
+    const [activeCategory, setActiveCategory] = useState(categories[0])
+
     return (
-        <>
-            <Link to="/login">HOME</Link>
-        </>
+        <div className="flex gap-2 justify-center md:justify-start">
+            {categories.map((category) => (
+                <div className="w-24 h-7 md:w-32 md:h-9 flex justify-center items-center text-sm">
+                    <Button 
+                        label={category} 
+                        variant={activeCategory === category ? 'default' : 'categories'} 
+                        onClick={() => setActiveCategory(category)}
+                    />
+                </div>
+            ))}
+        </div>
     )
 }
 
